6eb15066132a3639ad6f20a1db7b4a637a0586a6
David Straub <david.straub@tum.de>
2015-08-07 10:53:13 +0200

    Moved stuff from scripts to functions.

--- inspiretools/functions.py
+++ inspiretools/functions.py
@@ -1,5 +1,7 @@
 import re
 import os
+import pyinspire
+from lxml import etree as et
 
 def aux2texkey(filename):
     keys=[]
@@ -18,3 +20,63 @@ def aux2texkey(filename):
     # remove duplicates
     keys = list(set(keys))
     return keys
+
+def onabort():
+    print('The reference extraction has been aborted before completion.')
+
+def atend(err, notfound):
+    if err != '':
+        print('The following references could not be extracted due to errors:\n')
+        print(err)
+
+    if notfound != '':
+        print('The following references could not be found on INSPIRE:\n')
+        print(notfound)
+
+
+def texkey2bib(texkeys):
+    err=''
+    notfound=''
+    for texkey in texkeys:
+        try:
+            bib = pyinspire.get_text_from_inspire('texkey ' + texkey, 'bibtex')
+            if bib != '':
+                print(bib)
+            else:
+                notfound  += texkey + '\n'
+        except KeyboardInterrupt:
+            onabort()
+            break
+        except:
+            err += texkey + '\n'
+    atend(err, notfound)
+
+
+def texkey2xml(texkeys, tags=245):
+    err=''
+    notfound=''
+    first = True
+    for texkey in texkeys:
+        try:
+            bib = pyinspire.get_text_from_inspire('texkey ' + texkey, 'marcxml',
+                                                  ot=tags)
+            data = et.fromstring(bib.encode("utf-8"))
+            if data.find('{http://www.loc.gov/MARC21/slim}record') is not None:
+                if first:
+                    xml = data
+                    first = False
+                else:
+                    xml.extend(data)
+
+            else:
+                notfound  += texkey + '\n'
+        except KeyboardInterrupt:
+            onabort()
+            break
+        except:
+            err += texkey + '\n'
+
+    if not first:
+        print(et.tostring(xml, pretty_print=True).decode("utf-8"))
+
+    atend(err, notfound)
