3cbeeb5193783b1c7c73a60201211f9a174e7b3a
David Straub <david.straub@tum.de>
2015-08-07 10:34:54 +0200

    Added non-existent entry for test

--- inspiretools/test_functions.py
+++ inspiretools/test_functions.py
@@ -8,8 +8,8 @@ class TestParseaux(unittest.TestCase):
         """
         this_dir = os.path.dirname(os.path.realpath(__file__))
         filename = os.path.join(this_dir, '..', 'examples', 'example.aux')
-        filename
         keys = aux2texkey(filename)
         keys_correct = ["D'Agnolo:2012ie",'Straub:2013zca',
-                        'Straub:2013uoa','Barbieri:2014tja']
+                        'Straub:2013uoa','Barbieri:2014tja',
+                        'thisshouldntwork']
         self.assertCountEqual(keys,keys_correct)
