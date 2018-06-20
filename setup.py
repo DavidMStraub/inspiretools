421ed997f34705cd02268fc6baa9404cfe2beb24
David Straub <david.straub@tum.de>
2018-06-20 14:01:22 +0200

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='inspiretools',
-      version='0.1',
+      version='0.2',
       url='https://github.com/DavidMStraub/inspire-tools',
       author='David M. Straub',
       packages=['inspiretools'],
